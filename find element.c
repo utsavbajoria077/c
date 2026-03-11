bc7f8a72c91678bbb96f9c327661cbabe026f44d
utsavbajoria077 <utsavbajoria077@gmail.com>
2026-03-11 16:53:07 +0530

    Add element search program in find_element.c
    Implement a program to find an element in an array.

---
+++ find element.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter size of element");
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+int key,found;
+printf("\nenter element to search");
+scanf("%d",&key);
+for(int i=0;i<n;i++){
+    if (a[i]==key){
+        printf("element found at position  %d ",i+1);
+        found=1;
+        break;
+    }
+}
+if (found==0){
+    printf("element not found");
+}
+}
