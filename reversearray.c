4d2c1806fd1224c3b955f902255c32bc27d35ba7
utsavbajoria077 <utsavbajoria077@gmail.com>
2026-03-11 16:06:21 +0530

    Implement array reversal program in C
    This program reads an array of integers from user input and prints it in both original and reversed order.

---
+++ reversearray.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter size of element");
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+}
+for(int i=n-1;i>=0;i--){
+        printf("%d ",a[i]);
+}
+}
